4f6cea9235a9b6196c72a71b50ee1c8fa6f63c1d
Andrey Knyazev <andreyvknyazev1@gmail.com>
2021-09-23 12:39:50 -0400

    Initial Commit

---
+++ KK_App2/KK_App2App.swift
@@ -0,0 +1,17 @@
+//
+//  KK_App2App.swift
+//  KK_App2
+//
+//  Created by Andrey Knyazev on 9/23/21.
+//
+
+import SwiftUI
+
+@main
+struct KK_App2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
